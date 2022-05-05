90295971eaed6a2304219417a52b8cfe71130693
Luiz Paulo Caldas <luiz21.lupa@gmail.com>
2022-05-05 18:45:36 -0300

    adicionar novos recursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { CorrentistaComponent } from './components/correntista/correntista.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'movimentacoes-new', component: MovimentacaoNewComponent},
+  {path: 'movimentacoes', component: MovimentacaoListComponent},
+  {path: '', redirectTo: 'movimentacoes', pathMatch: 'full'},
+  {path: 'correntistas', component: CorrentistaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
